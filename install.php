0762158a49856227992253e82d480848214c6d10
Clement FOLTRAN <clfoltra@e1r7p1.42.fr>
2019-03-31 23:15:08 +0200

    finish

--- install.php
+++ install.php
@@ -1,25 +1,25 @@
 #!/usr/bin/php
 <?php
     include ('model/database.php');
-    $co = mysqli_connect("127.0.0.1", "root", "rushrush");
-    if (!$co)
+    $con = mysqli_connect("localhost:3307", "root", "rushrush", "test");
+    if (!$con)
         die("An error occured 😥\n");
     $db_query = "CREATE DATABASE rushdb";
-    if (mysqli_query($co, $db_query))
+    if (mysqli_query($con, $db_query))
         echo "Database created 👌 \n";
     else
         echo "An error occured 😥\n";
-    mysqli_close($co);
+    mysqli_close($con);
     
-    $co = connect();
+    $con = connect();
     $passwd = hash("whirlpool", 'pampata');
-    $all_query = file_get_contents('./SQL/create_tab.sql');
+    $all_query = file_get_contents('./SQL/create_tables.sql');
     $all_query .= file_get_contents('./SQL/populate.sql');
     $all_query .= "INSERT INTO users VALUES(id_user, 'phil', 'pamp', 'pampata', '$passwd', 'ok', 2000);\n";
     $all_query .= "INSERT INTO users VALUES(id_user, 'phil', 'pamp', 'pampata', '$passwd', 'ok', 2000);";
-    if (mysqli_multi_query($co, $all_query))
+    if (mysqli_multi_query($con, $all_query))
         echo "Create table successfully 👌 \n";
     else
         echo "An error occured 😥\n";
-    mysqli_close($co);
+    mysqli_close($con);
 ?>
--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
 	function connect() {
-	$host = "localhost";
+	$host = "localhost:3307";
 	$user = "root";
 	$pass = "rushrush";
 	$db = "rushdb";
